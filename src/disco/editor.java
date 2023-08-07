367bc0c7904b2698a3b38256c52429cd67c28646
Raphaela Lima Conti <16985763669@tco.lan>
2023-08-07 15:07:53 -0300

    socuero

--- src/disco/editor.java
+++ src/disco/editor.java
@@ -154,6 +154,8 @@ public class editor extends javax.swing.JFrame {
             } catch (IOException ex) {
                 Logger.getLogger(editor.class.getName()).log(Level.SEVERE, null, ex);
             }
+            
+            System.out.println("Suecuero :)");
 
         }
     }//GEN-LAST:event_btnSalvarActionPerformed
